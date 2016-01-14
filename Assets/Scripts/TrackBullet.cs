1fb857fa84e906fb12215db10292c91981422cbc
sunjiajian <1003512683@qq.com>
2016-01-14 18:22:46 +0800

    update

---
+++ Assets/Scripts/TrackBullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackBullet : MonoBehaviour {
+
+    public float speed = 4f;
+
+    private Enemy target;
+
+	// Use this for initialization
+	void Start () {
+        print("born");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(Vector3.up * speed * Time.deltaTime);
+        float dt = Time.deltaTime;
+
+        if (target != null) {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * dt);
+        }
+
+        if (transform.position.y > 6f || transform.position.y < -6f || transform.position.x > 5f || transform.position.x < -5f) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy") {
+            if (!other.GetComponent<Enemy>().isDead) {
+                other.gameObject.SendMessage("OnHit");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void printss() {
+        print("printss");
+    }
+
+    public void SetTarget(GameObject t) {
+        target = t.GetComponent<Enemy>();
+        print("SetTarget");
+    }
+}
---
+++ Assets/Scripts/TrackGun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackGun : MonoBehaviour {
+
+    public float rate = 2f;
+    private float tick = 0;
+
+    public float maxDistance = 1f;
+
+    public TrackBullet bullet;
+
+    GameObject minDisEnemy;
+
+	// Use this for initialization
+	void Start () {
+        rate = 2f;
+        tick = rate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tick -= Time.deltaTime;
+        if (tick <= 0) { 
+            GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+            if (enemys.Length > 0) {
+                float minDis = 1000000f;
+
+                foreach (var enemy in enemys){
+                    float dis = Vector3.Distance(transform.position, enemy.transform.position);
+                    minDis = minDis < dis ? dis : minDis;
+                    minDisEnemy = enemy;
+                }
+
+                if (minDis < maxDistance) {
+                    /*TrackBullet gobj = (TrackBullet)*/GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+                    //gobj.printss();
+                    print("finish born");
+                    //gobj.SetTarget(minDisEnemy);
+
+                    tick = rate;
+                }
+            }
+        }
+	}
+}
